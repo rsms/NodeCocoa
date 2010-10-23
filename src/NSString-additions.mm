57c3b04da6dc222b0e0b77cc291b9e3c5ced14ed
Rasmus Andersson <rasmus@notion.se>
2010-10-23 14:41:39 +0200

    house cleaning

---
+++ src/NSString-additions.mm
@@ -0,0 +1,13 @@
+//
+//  NSString-additions.m
+//  NodeCocoa
+//
+//  Created by Rasmus Andersson on 2010-10-23.
+//  Copyright 2010 Spotify. All rights reserved.
+//
+
+
+
+@implementation NSString_additions
+
+@end
